ed0a1c711d51a6d1f3cf4c022012cb993bdce4a2
KIRANSANKAREJ <kiransej98@gmail.com>
2024-09-18 11:56:14 +0100

    Changes to add dog

--- myFirstRScript.R
+++ myFirstRScript.R
@@ -1,3 +1,5 @@
-animals <- c("worm", "emu", "dog", "fly", "spider")
-num_legs <- c(0,2,4,6,8)
+animals <- c("worm", "cat", "dog", "fly", "spider")
+num_legs <- c(0,4,4,6,8)
 animals_df <- data.frame(animals, num_legs)
+
+animals_df
